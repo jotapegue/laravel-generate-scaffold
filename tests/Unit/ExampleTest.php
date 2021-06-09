857285ef79d5ebdab9616d375f3168f48d64fa46
João Pedro Garcia <jotapegue09@gmail.com>
2021-06-09 19:10:03 -0300

    make tests example

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jotapegue\GenerateScaffold\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
