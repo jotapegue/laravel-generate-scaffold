3f91ce82397d77c34da8ce492726c43e1b4a5f24
João Pedro Garcia <jotapegue09@gmail.com>
2021-06-10 11:13:09 -0300

    make scaffold service provider

---
+++ src/ScaffoldServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jotapegue\Scaffold;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScaffoldServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // 
+    }
+
+    public function register()
+    {
+        // 
+    }
+}
