9437843b1f711eb2e4b5455401227326d8087410
João Pedro Garcia <jotapegue09@gmail.com>
2021-06-10 11:13:53 -0300

    register command on boot package

--- src/ScaffoldServiceProvider.php
+++ src/ScaffoldServiceProvider.php
@@ -3,12 +3,17 @@
 namespace Jotapegue\Scaffold;
 
 use Illuminate\Support\ServiceProvider;
+use Jotapegue\Scaffold\Console\Commands\ScaffoldCommand;
 
 class ScaffoldServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // 
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ScaffoldCommand::class,
+            ]);
+        }
     }
 
     public function register()
